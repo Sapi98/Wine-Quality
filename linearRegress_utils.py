65d58539f53bd5058864e641fa217404256ea31c
Saptarshi Biswas <saptarshi.biswas9@gmail.com>
2021-02-27 19:02:02 -0600

    Codes added

--- linearRegress_utils.py
+++ linearRegress_utils.py
@@ -2,21 +2,28 @@ import numpy as np
 
 class Model:
     
-    def __init__(self, max_iter=100, alpha=0.01):
+    def __init__(self, max_iter=100, alpha=0.01, l=0):
         self.W = None
         self.max_iter = max_iter
         self.alpha = alpha
+        self.l = l
 
     def save_weight(self, file_name="weight.npy", path=None):
-        np.save()
+        if path != None:
+            np.save(path, self.W)
+        else:
+            np.save(file_name, self.W)
 
     def load_weight(self, file_name="weight.npy", path=None):
-        pass
+        if path != None:
+            self.W = np.load(path)
+        else:
+            self.W = np.load(file_name)
 
     def cost(self, X, y):
         pass
 
-    def gradient(self, X, y):
+    def gradient(self, cost, X, y):
         pass
 
     def update_weights(self, grad):
