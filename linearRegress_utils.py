3a8a210179414f259a0e28c7a5fb5ac7d61a4e8a
Saptarshi Biswas <saptarshi.biswas9@gmail.com>
2021-02-27 18:18:27 -0600

    architecture modified

--- linearRegress_utils.py
+++ linearRegress_utils.py
@@ -2,14 +2,24 @@ import numpy as np
 
 class Model:
     
-    def __init__(self, max_iter):
+    def __init__(self, max_iter=100, alpha=0.01):
         self.W = None
         self.max_iter = max_iter
+        self.alpha = alpha
 
     def save_weight(self, file_name="weight.npy", path=None):
         np.save()
 
     def load_weight(self, file_name="weight.npy", path=None):
+        pass
+
+    def cost(self, X, y):
+        pass
+
+    def gradient(self, X, y):
+        pass
+
+    def update_weights(self, grad):
         pass
 
     def fit(self, X, y):
