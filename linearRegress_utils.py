010e2a185e757b7a9568490559744b7103c90a14
Saptarshi Biswas <saptarshi.biswas9@gmail.com>
2021-02-27 18:15:15 -0600

    small changes

--- linearRegress_utils.py
+++ linearRegress_utils.py
@@ -2,11 +2,12 @@ import numpy as np
 
 class Model:
     
-    def __init__(self):
+    def __init__(self, max_iter):
         self.W = None
+        self.max_iter = max_iter
 
     def save_weight(self, file_name="weight.npy", path=None):
-        pass
+        np.save()
 
     def load_weight(self, file_name="weight.npy", path=None):
         pass
@@ -14,7 +15,7 @@ class Model:
     def fit(self, X, y):
         pass
 
-    def predict(self, x):
+    def predict(self, X):
         pass
 
     def evaluate(self, X, y):
